f03728bcbb278e2fc179e3c884d37aef1cdc3ace
Akhil Krishnan Namanattu Sunilkumar <akhilkrishnanns@live.com>
2023-03-25 19:59:02 +0000

    feat: initial commit with base files

---
+++ src/main/java/com/sterilecodewalker/customerms/sample/CustomerMsSampleApplication.java
@@ -0,0 +1,13 @@
+package com.sterilecodewalker.customerms.sample;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CustomerMsSampleApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CustomerMsSampleApplication.class, args);
+	}
+
+}
